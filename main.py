ecac009415848cd02891b0ab51f0bf2e966b14ec
Mikhail Skidan <mskidan@luxoft.com>
2021-11-29 00:25:52 +0300

    main.py style corrections

--- main.py
+++ main.py
@@ -8,40 +8,25 @@ from server import FileService
 
 
 def main():
-    argv = sys.argv
-    parser = argparse.ArgumentParser()
-    # parser.add_argument('-d', default='data', type=str, help="working directory (default: 'data')",
-    #                     required=False)
-    parser.add_argument('-d', '--dir', default='data', type=str, help="working directory (default: 'data')",
-                        required=False)
-    params = parser.parse_args()
-    logging.debug(FileService.get_current_dir())
-    # FileService.change_dir(".")
-    logging.debug("params: {}".format(params))
-
-    if bool(params.dir):
-        os.chdir(params.dir)
-    # if bool(params.d):
-    #     os.chdir(params.d)
-    # from server import FileService
-
-    reload(FileService)
-    # FileService.__init__()
-    logging.debug(FileService.get_current_dir())
-    FileService.change_dir(".")
-
     """
     Command line options:
     -d --dir  - working directory (absolute or relative path, default: current_app_folder/data).
     -h --help - help.
     """
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-d', '--dir', default='data', type=str,
-    #                     help="working directory (default: 'data')")
-    # params = parser.parse_args()
-    #
-    # work_dir = params.dir if os.path.isabs(params.dir) else os.path.join(os.getcwd(), params.dir)
-    # FileService.change_dir(work_dir)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir', default='data', type=str, help="working directory (default: 'data')",
+                        required=False)
+    params = parser.parse_args()
+    logging.debug(f'FileService current dir: {FileService.get_current_dir()}')
+    logging.debug("params: {}".format(params))
+
+    if bool(params.dir):
+        os.chdir(params.dir)
+
+    reload(FileService)
+    logging.debug(f'FileService current dir: {FileService.get_current_dir()}')
+    FileService.change_dir(".")
 
 
 if __name__ == '__main__':
